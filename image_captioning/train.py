663a6d2a6df9289b39a69e93b29bcfe535112579
Gurbaaz Singh Nandra <60743677+gurbaaz27@users.noreply.github.com>
2020-06-25 02:20:58 +0530

    Corrected default args

--- image_captioning/train.py
+++ image_captioning/train.py
@@ -19,9 +19,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-model',type=str,default='resnet18')
     parser.add_argument('-dir',type=str,default='train')
-    parser.add_argument('-save_epoch',type=int,default=1)
+    parser.add_argument('-save_epoch',type=int,default=2)
     parser.add_argument('-learning_rate',type=float,default=1e-3)
-    parser.add_argument('-num_epoch',type=int,default=1)
+    parser.add_argument('-num_epoch',type=int,default=10)
     parser.add_argument('-hidden_dim',type=int,default=512)
     parser.add_argument('-embedding_dim',type=int,default=512)
 
